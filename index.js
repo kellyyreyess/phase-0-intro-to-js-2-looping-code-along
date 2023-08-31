9d3556b05e905b6ee43f432823f031146b469af0
Kelly Reyes <kellyreyes219@gmail.com>
2023-08-31 18:54:06 -0400

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) { 
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+//     return gifts;
+// }
+
+const cards = ["Birthday", "Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) { 
+    const messages = []
+    for (let c = 0; c < cards.length; c++) {
+        console.log(`Thank you, ${cards[c]}, for the wonderful surprise gift!`);
+        messages.push(`Thank you, ${cards[c]}, for the wonderful surprise gift!`);
+
+    }
+    return messages;
+ }
+   
+
+console.log(writeCards(cards)) 
+
+function countDown() { 
+    let countDown = 10;
+    while (countDown > -1) {
+        console.log(countDown--)
+    }
+}
+  
+
